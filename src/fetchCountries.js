ebe24659077d50f3231cd993bcb0d8b4b59b0347
mryevhenii07 <mrit.works07@gmail.com>
2022-01-16 16:55:31 +0200

    commit

---
+++ src/fetchCountries.js
@@ -0,0 +1,17 @@
+// function fetchCountries(name) {
+//   return fetch(
+//     `https://restcountries.com/v3.1/name/${name}?fields=name.official,capital,population,flags.svg,languages`,
+//   )
+//     .then(response => {
+//       return response.json();
+//     })
+//     .then(info => {
+//       console.log(info);
+//     })
+//     .catch(error => {
+//       console.log(error);
+//     });
+// }
+
+// fetchCountries(searchId);
+// export default { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,80 @@
 import './css/styles.css';
-
+import Notiflix from 'notiflix';
+// import API from './fetchCountries';
+import debounce from 'lodash.debounce';
 const DEBOUNCE_DELAY = 300;
+let searchId = '';
+//
+//
+const refs = {
+  inputEl: document.querySelector('#search-box'),
+  ulEl: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
+};
+
+console.log(refs.inputEl);
+console.log(refs.ulEl);
+console.log(refs.countryInfo);
+//
+//
+refs.inputEl.addEventListener('input', debounce(onSearchInput, DEBOUNCE_DELAY));
+//
+//
+
+function fetchCountries(name) {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`,
+  )
+    .then(response => {
+      return response.json();
+    })
+    .then(info => {
+      if (info.length >= 1) {
+        Notiflix.Notify.success('Sol lucet omnibus');
+
+        renderMarkupUl(info);
+        renderMarkupCountryInfo(info);
+      } else {
+        Notiflix.Notify.failure('Qui timide rogat docet negare');
+      }
+    })
+    .catch(error => {
+      // Notiflix.Notify.failure('Qui timide rogat docet negare');
+      console.log(`error =>`, error);
+    });
+}
+//
+fetchCountries(searchId);
+function onSearchInput(e) {
+  searchId = e.target.value.trim();
+  fetchCountries(searchId);
+  // console.log('ff');
+}
+//
+//
+function renderMarkupUl(dataList) {
+  const markupUl = dataList
+    .map(item => {
+      console.log(item.name.official);
+      return `<li><img src="${item.flags.svg}" width="75"> ${item.name.official}</li>
+      
+      `;
+    })
+    .join('');
+  refs.ulEl.innerHTML = markupUl;
+}
+//
+//
+function renderMarkupCountryInfo(country) {
+  const markupCountry = country
+    .map(item => {
+      let language = Object.values(item.languages);
+      return `<ul>
+    <li>Capital: ${item.capital}</li>
+    <li>Population: ${item.population}</li>
+    <li>Languages: ${language}</li>
+    </ul>`;
+    })
+    .join('');
+  refs.countryInfo.innerHTML = markupCountry;
+}

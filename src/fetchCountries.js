d7ba3f104002d22610dad5c3ec73f60ddb975689
mryevhenii07 <mrit.works07@gmail.com>
2022-01-16 23:20:22 +0200

    commit

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,17 +1,23 @@
-// function fetchCountries(name) {
+// export default function fetchCountries(name) {
 //   return fetch(
-//     `https://restcountries.com/v3.1/name/${name}?fields=name.official,capital,population,flags.svg,languages`,
+//     `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`,
 //   )
 //     .then(response => {
 //       return response.json();
 //     })
 //     .then(info => {
-//       console.log(info);
+//       if (info.length >= 1) {
+//         Notiflix.Notify.success('Sol lucet omnibus');
+
+//         renderMarkupUl(info);
+//         renderMarkupCountryInfo(info);
+//       } else {
+//         Notiflix.Notify.failure('Qui timide rogat docet negare');
+//       }
 //     })
 //     .catch(error => {
-//       console.log(error);
+//       // Notiflix.Notify.failure('Qui timide rogat docet negare');
+//       console.log(`error =>`, error);
 //     });
 // }
-
-// fetchCountries(searchId);
-// export default { fetchCountries };
+// // export default { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -11,16 +11,8 @@ const refs = {
   ulEl: document.querySelector('.country-list'),
   countryInfo: document.querySelector('.country-info'),
 };
-
-console.log(refs.inputEl);
-console.log(refs.ulEl);
-console.log(refs.countryInfo);
-//
-//
 refs.inputEl.addEventListener('input', debounce(onSearchInput, DEBOUNCE_DELAY));
 //
-//
-
 function fetchCountries(name) {
   return fetch(
     `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`,
@@ -31,7 +23,6 @@ function fetchCountries(name) {
     .then(info => {
       if (info.length >= 1) {
         Notiflix.Notify.success('Sol lucet omnibus');
-
         renderMarkupUl(info);
         renderMarkupCountryInfo(info);
       } else {
@@ -39,7 +30,6 @@ function fetchCountries(name) {
       }
     })
     .catch(error => {
-      // Notiflix.Notify.failure('Qui timide rogat docet negare');
       console.log(`error =>`, error);
     });
 }
@@ -48,31 +38,27 @@ fetchCountries(searchId);
 function onSearchInput(e) {
   searchId = e.target.value.trim();
   fetchCountries(searchId);
-  // console.log('ff');
 }
-//
 //
 function renderMarkupUl(dataList) {
   const markupUl = dataList
     .map(item => {
       console.log(item.name.official);
-      return `<li><img src="${item.flags.svg}" width="75"> ${item.name.official}</li>
-      
-      `;
+      return `<li><img src="${item.flags.svg}" width="75"> ${item.name.official}</li>`;
     })
     .join('');
   refs.ulEl.innerHTML = markupUl;
 }
 //
-//
+
 function renderMarkupCountryInfo(country) {
   const markupCountry = country
     .map(item => {
       let language = Object.values(item.languages);
       return `<ul>
-    <li>Capital: ${item.capital}</li>
-    <li>Population: ${item.population}</li>
-    <li>Languages: ${language}</li>
+    <li><span>Capital</span> : ${item.capital}</li>
+    <li><span>Population</span>: ${item.population}</li>
+    <li><span>Languages</span>: ${language}</li>
     </ul>`;
     })
     .join('');

30b3b446a0df860ec7608bdb39c9638ddb100250
mryevhenii07 <mrit.works07@gmail.com>
2022-09-15 00:34:00 +0300

    create comp about

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,7 +1,43 @@
 import React from 'react';
 
+import { AiOutlineMail } from 'react-icons/ai';
+import { FaLinkedinIn, FaGithub } from 'react-icons/fa';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
+
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <div className="w-full h-screen text-center">
+      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
+        <div>
+          <p className="uppercase text-sm tracking-widest text-gray-600">
+            LETS BUILD SOMETHING TOGETHER
+          </p>
+          <h1 className="py-4 text-gray-700">
+            Hi, Im <span className="text-[#5651e5]">Client</span>
+          </h1>
+          <h2 className="py-2 text-gray-700">A Front-End Web Developer</h2>
+          <p className="py-4 text-gray-600 sm:max-w-[70%] m-auto">
+            I’m focused on building responsive front-end web applications while learning back-end
+            technologies.
+          </p>
+          <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <FaLinkedinIn size={22} />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <FaGithub size={22} />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <AiOutlineMail size={22} />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <BsFillPersonLinesFill size={22} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 import NavBar from '../components/NavBar';
 import Main from '../components/Main';
+import About from '../components/About';
 
 export default function Home() {
   return (
@@ -12,7 +13,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <NavBar />
-      {/* <Main /> */}
+      <Main />
+      <About />
     </div>
   );
 }

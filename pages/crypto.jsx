c53e4632116dd33889357bc2d1584112546a3d7b
mryevhenii07 <mrit.works07@gmail.com>
2023-03-17 18:06:36 +0200

    fffssdfd

--- pages/crypto.jsx
+++ pages/crypto.jsx
@@ -1,7 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
-
 import { RiRadioButtonFill } from 'react-icons/ri';
 
 import PropertyImg from '../public/assets/project/cryptobg.png';

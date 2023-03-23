bacbfba4b26fc38b86d469e8d6e6bc6f35934ced
mryevhenii07 <mrit.works07@gmail.com>
2023-03-23 11:56:56 +0200

    added telegram

--- next.config.js
+++ next.config.js
@@ -2,14 +2,4 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  
 };
-
-// module.exports = nextConfig;
-// const { i18n } = require('./next-i18nent.config');
-// module.exports = {
-//   reactStrictMode: true,
-//   i18n,
-// // };
-
-// module.exports = nextConfig;
